64ff23196821715fc9d6db2df6e9178a8fc15a38
DanielGutierrez20 <n00507628@upn.pe>
2026-09-21 00:12:09 -0500

    req2: agrega validacion de longitud minima para codigo de estudiante

--- Program.cs
+++ Program.cs
@@ -13,7 +13,7 @@ public class Program
         Console.WriteLine("3. Salir");
         Console.WriteLine("=======================================");
     }
-    
+
     //Función validar texto no vacío
     static string ObtenerTextoValido(string mensaje, int minLongitud)
     {
@@ -32,12 +32,14 @@ public class Program
     }
 	public static void Main()
 	{
-		// COMMIT REQ 1: Estrutura base de datos
+		//Estructura
         const int MAX_SOLICITUDES = 3;
         string[] codigos = new string[MAX_SOLICITUDES];
         string[] nombres = new string[MAX_SOLICITUDES];
         string[] tipos = new string[MAX_SOLICITUDES];
         string[] descripciones = new string[MAX_SOLICITUDES];
         string[] prioridades = new string[MAX_SOLICITUDES];
+        
+        string codigo = ObtenerTextoValido("Ingrese código de estudiante (mín. 5 caract.): ", 5);
 	}
 }
