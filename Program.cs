f5723692711b179a6b50ccc489a01c0ad810bb8c
DanielGutierrez20 <n00507628@upn.pe>
2026-09-21 00:03:11 -0500

    req1: crea estructura base para entrada de datos de solicitud

--- Program.cs
+++ Program.cs
@@ -3,6 +3,12 @@ public class Program
 {
 	public static void Main()
 	{
-		Console.WriteLine("Hello World");
+		// COMMIT REQ 1: Estrutura base de datos
+        const int MAX_SOLICITUDES = 3;
+        string[] codigos = new string[MAX_SOLICITUDES];
+        string[] nombres = new string[MAX_SOLICITUDES];
+        string[] tipos = new string[MAX_SOLICITUDES];
+        string[] descripciones = new string[MAX_SOLICITUDES];
+        string[] prioridades = new string[MAX_SOLICITUDES];
 	}
 }
